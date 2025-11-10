89850583fda863c706fda11c03f8333b993f4ce6
Marlee <marleemooskie@gmail.com>
2025-11-10 08:43:49 -0700

    adding some temperature checks

--- QAQC.py
+++ QAQC.py
@@ -1,13 +1,24 @@
 '''
-In this script, I will load the data and attempt to start fitting my first RNN
-or ESN to it. I will also investigate the days surrounding a heatwave.
+In this script, I will do QAQC including:
+    - inspecting site variable values and removing missing areas
+    - correcting negative values that shouldn't be there
+    - comparing temperature values in PRISM and AmeriFlux data for consistent
+    
+Currently, I am compring quantiles between PRISM and AMF for max temperatures. 
+They align at most the sites, except two, which I am investigating month specific 
+misalignment. Next I need to do this for minimum and average temperatures, but 
+first I will need to acquire the PRISM for these measrements.
 
+Also, we need PRISM data for Canada too.
 '''
 
 import os
 os.chdir("/Users/marleeyork/Documents/project2")
 from load_data import *
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
 pd.set_option('display.max_columns',300)
 pd.set_option('display.max_rows',100)
 
@@ -15,6 +26,9 @@ pd.set_option('display.max_rows',100)
 # This automatically loads selected for columns, defined in loadAMF
 df = loadAMF(path='/Users/marleeyork/Documents/project2/data/AMFdataDD',
                  measures=['TIMESTAMP','TA_F','SW_IN_F','VPD_F','P_F','NEE_VUT_REF','RECO_NT_VUT_REF','GPP_NT_VUT_REF','SWC_F_MDS_1'])
+
+df_hourly = loadAMF(path='/Users/marleeyork/Documents/project2/data/AMFdata_HH',
+                 measures=['TIMESTAMP_START','TA_F'])
 
 # Load the IGBP data and merge to df
 site_data = pd.read_csv("/Users/marleeyork/Documents/project2/data/site_list.csv",encoding='latin1')
@@ -34,6 +48,9 @@ df.IGBP.unique()
 # Drop any rows with values that are -9999 (new shape is (190298,11))
 mask = df.apply(lambda col: col == -9999).any(axis=1)
 df = df[~mask]
+
+# Dropping US-Ne1, not sure why its in here
+df = df[df['IGBP']!='CRO']
 
 # If GPP is negative, set it to 0
 df.loc[df['GPP_NT_VUT_REF']<0,'GPP_NT_VUT_REF'] = 0
@@ -107,4 +124,129 @@ for site in df.Site.unique():
     plt.show()
     
     input("Press [Enter] to continue...")
+    
+###############################################################################
+##                       TEMPERATURE CHECKS                                  ##
+###############################################################################
+
+# Comparing max temperature PRISM to AmeriFlux
+# Right now, I have this available across all 33 sites!
+
+# Load in the data
+historical_max = pd.read_csv("/Users/marleeyork/Documents/project2/data/PRISM/extracted_daily_climate_data_tmax.csv")
+
+# Drop any sites that aren't in df
+included_sites = df.Site.unique()
+included_sites = np.insert(included_sites,0,'date')
+historical_max = historical_max[included_sites]
+
+# Findig which sites have missing values
+search_value = -9999
+columns_with_value = []
+for col in historical_max.columns:
+    # Check if the search_value exists in the current column
+    if historical_max[col].astype(str).str.contains(str(search_value)).any():
+        columns_with_value.append(col)
+        
+print(f"Sites that we don't have PRISM data include: {columns_with_value}")
+
+# These columns are completely missing all PRISM temperature, interesting
+# As of now, its the Canada sites we are missing data for (PRISM doesn't go to Canada)
+# and two US sites that are in Alaska
+historical_max[columns_with_value]
+
+# Starting off with the ones that we do know
+df_max = historical_max.drop(columns=columns_with_value)
+
+# Pivoting df_max longer
+df_max = df_max.melt(id_vars=['date'],var_name='Site',value_name='max_PRISM')
+
+# Now I am going to calculate the daily maximum temperature for each of my sites
+# using my function as done in my heatwave definition
+AMF_max = pd.DataFrame(columns=['Site','date','max_temperature'])
+for site in df_max.Site.unique():
+    this_site = df_hourly[df_hourly['Site']==site]
+    this_site_temp = find_max_temperatures(this_site.TIMESTAMP_START,this_site.TA_F)
+    this_site_temp['Site'] = [site] * this_site_temp.shape[0]
+    # concatenate with entire dataframe
+    AMF_max = pd.concat([AMF_max,this_site_temp])
+    
+# Convert df_max date to a datetime variable
+df_max.date = pd.to_datetime(df_max.date)
+
+# Merging df and df max
+# df.shape = (159306, 11)
+# df_max.shape = (15705, 24)
+# df_max after the merge = (140984, 4)
+df_max = pd.merge(AMF_max, df_max, on=['Site','date'])
+
+# Lets look at the overall correlation between the PRISM and AmeriFlux data now
+fig, ax = plt.subplots()
+plt.scatter(df_max.max_temperature,df_max.max_PRISM, s=.5,alpha=.1)
+plt.xlabel("AMF Max Temperature")
+plt.ylabel("PRISM Max Temperature")
+plt.show()
+
+# Checking the overall correlation coefficient
+# Correlation is 93%, which is pretty good
+np.corrcoef(df_max.max_temperature,df_max.max_PRISM)
+
+# Checking site by site correlation to look for any anomalies
+# So far ONA and KFS have the weakest correlation, which is mid 80s. Everything
+# else is fine.
+for site in df_max.Site.unique():
+    this_site = df_max[df_max['Site']==site]
+    print(f"Correlation for site {site} is {np.corrcoef(this_site.max_temperature,this_site.max_PRISM)[0][1]}")
+
+# Finding the 95th percentile for each
+for site in df_max.Site.unique():
+    this_site = df_max[df_max['Site']==site]
+    # Calculate the 95th quantile overall
+    AMF_95 = np.quantile(this_site.max_temperature,.95)
+    PRISM_95 = np.quantile(this_site.max_PRISM,.95)
+    print(f"95th quantiles at site {site} are {AMF_95} and {PRISM_95}.")
+    
+# Find correlation between daily 95th quantile values
+# So far, these 
+daily_quantiles = pd.DataFrame(columns=['Site','month_day','AMF_quantiles','PRISM_quantiles'])
+for site in df_max.Site.unique():
+    this_site = df_max[df_max['Site']==site]
+    # Calculate window quantiles for each
+    AMF_daily_95 = moving_window_quantile(this_site.date,this_site.max_temperature,.95, 15)
+    AMF_daily_95.columns = ["month_day","AMF_quantiles"]
+    PRISM_daily_95 = moving_window_quantile(this_site.date,this_site.max_PRISM,.95,15)
+    PRISM_daily_95.columns = ["month_day","PRISM_quantiles"]
+    # Merge these together
+    window_quantiles = pd.merge(AMF_daily_95,PRISM_daily_95,on='month_day',how='inner')
+    # Add a site columns
+    window_quantiles['Site'] = [site] * window_quantiles.shape[0]
+    # Stack it onto the dataframe across all sites
+    daily_quantiles = pd.concat([daily_quantiles,window_quantiles])
+    # Print off the correlation coefficient
+    print(f"Correlation coefficient for site {site} is {np.corrcoef(daily_quantiles.AMF_quantiles,daily_quantiles.PRISM_quantiles)[0][1]}")
+    
+# Plotting the AMF and PRISM 95th moving quantiles
+fig, ax = plt.subplots()
+sb.lmplot(x='AMF_quantiles', y='PRISM_quantiles', data=daily_quantiles[daily_quantiles['Site']=='US-Mo2'], hue='Site', fit_reg=False)
+plt.show()
+
+# Checking the above to see if its a certain month or season we should be worried about
+KFS_quantiles = daily_quantiles[daily_quantiles['Site']=='US-KFS']
+KFS_quantiles.month_day = pd.to_datetime(KFS_quantiles.month_day,format='%m-%d')
+fig, ax = plt.subplots()
+plt.scatter(KFS_quantiles.month_day, KFS_quantiles.AMF_quantiles,c='red',s=.5)
+plt.scatter(KFS_quantiles.month_day, KFS_quantiles.PRISM_quantiles,c='blue',s=.5)
+plt.show()
+
+Mo2_quantiles = daily_quantiles[daily_quantiles['Site']=='US-Mo2']
+Mo2_quantiles.month_day = pd.to_datetime(Mo2_quantiles.month_day,format='%m-%d')
+fig, ax = plt.subplots()
+plt.scatter(Mo2_quantiles.month_day, Mo2_quantiles.AMF_quantiles,c='red',s=.5)
+plt.scatter(Mo2_quantiles.month_day, Mo2_quantiles.PRISM_quantiles,c='blue',s=.5)
+plt.show()
+
+
+
+
+
    
